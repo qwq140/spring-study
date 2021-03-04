4c6bd7c74c9b259251ef289c406817bade8165f3
admin <qwq140@gmail.com>
2021-03-04 15:03:10 +0900

    3. 스프링부트 JPA 회원가입,로그인,글쓰기/ EAGER,LAZY 개념(저장안한거 추가)

--- myjpa/src/main/java/com/cos/myjpa/domain/user/User.java
+++ myjpa/src/main/java/com/cos/myjpa/domain/user/User.java
@@ -1,15 +1,20 @@
 package com.cos.myjpa.domain.user;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.cos.myjpa.domain.post.Post;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,4 +35,7 @@ public class User {
 	private String email;
 	@CreationTimestamp // 자동으로 현재시간이 들어감.
 	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY) // 난 연관관계의 주인이 아니야. 그러니까 FK 만들지마.
+	private List<Post> posts;
 }
